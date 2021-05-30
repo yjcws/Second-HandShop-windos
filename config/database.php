628b13790ce423b85288b828f270777d02b0991b
yjcws <53468707+yjcws@users.noreply.github.com>
2021-05-30 13:05:21 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,13 +1,5 @@
 <?php
-//return [
-//    "mysql" => [
-//        "host" => "101.200.41.146",
-//        "port" => 3306,
-//        "user" => "www_itziliao123_",
-//        "password" => "piZdFdpKWZSEsCJs",
-//        "dbname" => 'www_itziliao123_'
-//    ],
-//];
+
 
 return [
     "mysql" => [
